395a3439dae7409fcb45eaf0e619b9f1c33ca679
Steven Esser <steven.michael.esser@gmail.com>
2014-07-09 15:17:17 -0700

    added start time calculation (prelim)

--- lib/importers/done_cards.rb
+++ lib/importers/done_cards.rb
@@ -5,7 +5,16 @@ module Importers
     def self.import(trello_done_list_id)
       cards = TrelloApi.cards_in_list(trello_done_list_id) || []
       cards.each_with_index do |done_card_hash, index|
+        # creates cards correctly
         done_card = DoneCard.find_or_create(done_card_hash)
+	# try to parse out the date
+	actions = TrelloApi.movement_actions_for_card(done_card.trello_id) || []
+	actions.each do |action_hash|
+          case action_hash[:type]
+	  when 'createCard'
+	    done_card.start = Chronic.parse(action_hash[:date])
+	  end
+	end
       end
     end
   end
